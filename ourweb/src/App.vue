429827f038ac5401c4bfffaa1b3ec2fa66f353d8
Tim S <84805291+tm-sm@users.noreply.github.com>
2023-10-12 10:57:23 -0300

    adds routing

--- ourweb/src/App.vue
+++ ourweb/src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <v-app>
-    <MiRutinaEditPage/>
+  <div id="app">
+      <NavigationBar />
+      <router-view v-slot="{ Component }">
+          <transition name="slide" mode="out-in">
+              <component :is="Component" :key="$route.path" />
+          </transition>
+      </router-view>
+  </div>
   </v-app>
 </template>
 
 <script setup>
   import MiRutinaEditPage from './pages/MiRutinaEditPage.vue';
-import HomePage from './pages/HomePage.vue'
+  import HomePage from './pages/HomePage.vue'
   import LoginPage from './pages/LoginPage.vue'
   import RegisterPage from './pages/RegisterPage.vue'
+  import NavigationBar from './components/MyHeader.vue'
 </script>
