7e3ef8375a8421f765996bafffb87e35c21c0646
kndtxt <csilvadiniz@itba.edu.ar>
2023-10-23 10:14:33 -0300

    Final commit 2 xd

--- ourweb/src/pages/MyRutinaViewPublic.vue
+++ ourweb/src/pages/MyRutinaViewPublic.vue
@@ -3,7 +3,7 @@
       <v-main style="padding-top: 110px; padding-bottom: 74px;">
         <MyHeader />
         <div class="subheader">
-          <MySubheaderRutina/>
+          <MySubheaderRutinasExplorar/>
         </div>
         <MiRutinaInfoPublic/>
         <MyFooter />      
@@ -15,13 +15,13 @@
 import MiRutinaInfoPublic from '@/components/MiRutinaInfoPublic.vue';
 import MyHeader from '../components/MyHeader.vue'
 import MyFooter from '@/components/MyFooter.vue';
-import MySubheaderRutina from '@/components/subheaders/MySubheaderRutina.vue';
+import MySubheaderRutinasExplorar from '@/components/subheaders/MySubheaderRutinasExplorar.vue';
   </script>
   
   <style scoped>
       .subheader{
           position: sticky;
-          top: 110px;
+          top: 104px;
           z-index: 999;
   
           background-color: #FFFFFF;
